f24bb0bea901a4e7f8e7883d952d39a4072c4440
Dennis Kardos Zetterstrøm <dennisz@viken.no>
2022-11-25 10:13:54 +0100

    Add project files.

---
+++ Kodeklubben1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kodeklubben1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Oppgave6();
+        }
+
+        static void Oppgave6()
+        {
+           
+            Console.WriteLine();
+        }
+    }
+
+}
